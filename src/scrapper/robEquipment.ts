a8a058a2bcf0236ee12a52ba25c60d72f8b6bc3d
Alamin Shaikh <hello@octib.com>
2023-03-23 12:34:46 +0600

    Rob equipment trucks added to db

--- src/scrapper/robEquipment.ts
+++ src/scrapper/robEquipment.ts
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer";
+import Truck from "../models/truck";
 import { sendErrorEmail } from "../utils";
 
 export default async function scrapRobEquipment() {
@@ -40,9 +41,13 @@ export default async function scrapRobEquipment() {
             // Add urls to truckUrls
             truckUrls = [...truckUrls, ...truckUrlsPerPage];
           } catch (err) {
+            // Close the browser and send email
+            await browser.close();
             sendErrorEmail("Rob Equipment");
           }
         } catch (err) {
+          // Close the browser and send email
+          await browser.close();
           sendErrorEmail("Rob Equipment");
         }
       }
@@ -125,18 +130,45 @@ export default async function scrapRobEquipment() {
             // Add truck to trucks
             trucks = [...trucks, truck];
           } catch (err) {
+            // Close the browser and send email
+            await browser.close();
             sendErrorEmail("Rob Equipment");
           }
         } catch (err) {
+          // Close the browser and send email
+          await browser.close();
           sendErrorEmail("Rob Equipment");
         }
       }
 
-      console.log(trucks);
+      // Replace the trucks in the db
+      try {
+        // Delete all previous trucks
+        await Truck.deleteMany({
+          website: "rsef",
+        });
 
-      // Close the browser
-      await browser.close();
+        try {
+          // Create new trucks
+          await Truck.create(trucks);
+
+          console.log("done");
+
+          // Close the browser
+          await browser.close();
+        } catch (err) {
+          // Close the browser and send email
+          await browser.close();
+          sendErrorEmail("Rob Equipment");
+        }
+      } catch (err) {
+        // Close the browser and send email
+        await browser.close();
+        sendErrorEmail("Rob Equipment");
+      }
     } catch (err) {
+      // Close the browser and send email
+      await browser.close();
       sendErrorEmail("Rob Equipment");
     }
   } catch (err) {
